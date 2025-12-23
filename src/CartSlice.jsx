a841718b137be8666dfb7b130e192d3aa3983d12
karv25-ui <karvenda@gmail.com>
2025-12-23 10:20:41 -0500

    codes

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,6 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import { updateQuantity, removeItem } from './CartSlice.jsx';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CartSlice';
+
+// Create a Redux store using configureStore from Redux Toolkit
+const store = configureStore({
+    // Define the root reducer object
+    reducer: {
+        // 'cart' is the name of the slice in the store, and it's managed by cartReducer
+        cart: cartReducer,
+    },
+});
+
 
 const dispatch = useDispatch();
 
